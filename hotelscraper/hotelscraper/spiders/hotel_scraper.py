dc029a4e5037eb008af8369238fc5c0f5ed11987
w3-software-intern-Riad <wengineerssoftwareengineer@gmail.com>
2024-08-08 17:52:36 +0600

    second commit

--- hotelscraper/hotelscraper/spiders/hotel_scraper.py
+++ hotelscraper/hotelscraper/spiders/hotel_scraper.py
@@ -16,5 +16,24 @@ class HotelScraper(scrapy.Spider):
         # Print the entire HTML for debugging purposes
         #
         script_content = response.css('script[type="text/javascript"]::text').get()
-   
-        yield {"title":script_content}
+# Use regex to extract the JSON-like part from the JavaScript variable
+        if 'window.IBU_HOTEL' in script_content:
+            # Use regex to extract the JSON-like object
+            json_string_match = re.search(r'window.IBU_HOTEL\s*=\s*({.*?});', script_content, re.DOTALL)
+            
+            if json_string_match:
+                json_string = json_string_match.group(1)
+                
+                # Convert the string to a Python dictionary
+                hotel_data = json.loads(json_string)
+                
+                # Now, you can access any value within the dictionary
+                init_data = hotel_data.get('initData', {})
+                seo_content = init_data.get('recommends', {})
+                
+            
+                
+                yield {
+                    'title1': init_data,
+                    'content1': seo_content,
+                }
